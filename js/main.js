7b9a3ef38da4822baf503c934ca513c6b8ccb2a8
klepik-ann <klepik1597@gmail.com>
2022-01-14 23:52:26 +0700

    pop-up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
 	$('.menuToggle').click(function () {
-		$(this).toggleClass('active');
+		$('.menuToggle,.header_body,header').toggleClass('active');
+
 		$('.header_list').slideToggle(300);
 		$('.call').slideToggle(300);
 	})
 });
+/*pop up */
+const openPopUp = document.getElementsByClassName('open_pop_up');
+const closePopUp = document.getElementsByClassName('pop_up_close');
+const popUp = document.getElementsByClassName('pop_up');
 
+openPopUp.addEventListener('click', function (e) {
+	e.preventDefault();
+	popUp.classList.add('active');
+});
 
+closePopUp.addEventListener('click', () => {
+	popUp.classList.remove('active');
+})
