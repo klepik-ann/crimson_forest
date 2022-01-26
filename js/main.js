d1380067c2bdf325eb89b23e1ad85412c216df5c
klepik-ann <klepik1597@gmail.com>
2022-01-26 17:49:47 +0700

    добавлен реальный текст

--- js/main.js
+++ js/main.js
@@ -6,16 +6,56 @@ $(document).ready(function () {
 		$('.call').slideToggle(300);
 	})
 });
-/*pop up */
-const openPopUp = document.getElementsByClassName('open_pop_up');
-const closePopUp = document.getElementsByClassName('pop_up_close');
-const popUp = document.getElementsByClassName('pop_up');
 
-openPopUp.addEventListener('click', function (e) {
-	e.preventDefault();
-	popUp.classList.add('active');
+
+/*pop up */
+
+
+/*Рабочий варик */
+// const openPopUp = document.getElementById('open_pop_up');
+// const closePopUp = document.getElementById('pop_up_close');
+// const popUp = document.getElementById('pop_up');
+
+// openPopUp.addEventListener('click', function (e) {
+// 	e.preventDefault();
+// 	popUp.classList.add('active');
+// });
+
+// closePopUp.addEventListener('click', () => {
+// 	popUp.classList.remove('active');
+// });
+
+let popUp = document.querySelector('.pop_up');
+let openPopup = document.querySelectorAll('.open_pop_up');
+let closePopUp = document.querySelector('.pop_up_close');
+
+openPopup.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		e.preventDefault();
+		popUp.classList.add('active');
+
+	});
 });
 
 closePopUp.addEventListener('click', () => {
 	popUp.classList.remove('active');
-})
+});
+
+
+/*кнопка подробнее */
+
+function readMore() {
+	let dots = document.getElementById("dots");
+	let more = document.getElementById("more");
+	let botn = document.getElementById("button_more");
+
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		botn.innerHTML = "Подробнее";
+		more.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		botn.innerHTML = "Скрыть";
+		more.style.display = "inline";
+	}
+};
